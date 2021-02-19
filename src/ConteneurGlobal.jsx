90619919b2d32900bb3c61bddf2ffc7333fd4a4a
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-02-19 15:49:26 -0500

    Obtenir l'information d'un tableau pour panier

--- src/ConteneurGlobal.jsx
+++ src/ConteneurGlobal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './ConteneurGlobal.scss';
 import Entete from './Entete';
 import ListeProduits from './ListeProduits';
@@ -11,12 +12,15 @@ export default function ConteneurGlobal() {
     "prd0851" : {prix: 12.67, qte: 15}
   } */
 
-  //Utiliser la gestion de l'état avec useState
-  let panier = {};
-  
+  //Utiliser la gestion de l'état avec "useState"
+  const etatPanier = useState({ "prd0002" : {prix: 17.88, qte: 2},
+                                "prd0025" : {prix: 9.99, qte: 1},
+                                "prd0851" : {prix: 12.67, qte: 1}});
+  console.log("État Panier", etatPanier);
+
   return (
     <div className="ConteneurGlobal">
-      <Entete />
+      <Entete etatPanier={etatPanier} />
        <section className="contenuPrincipal">
           <ListeProduits />
        </section>
--- src/Entete.jsx
+++ src/Entete.jsx
@@ -3,6 +3,24 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Badge } from '@material-ui/core';
 
 function Entete(props) {
+    console.log("Etat panier dans l'Entête :", props.etatPanier);
+    // Notez la "décomposition" de tableau
+    const [panier, setPanier] = props.etatPanier;
+    console.log("Le panier :", panier);
+    const articlesTab = Object.values(panier);
+    console.log("Les articles dans un tableau :", articlesTab);
+
+    // //Façon IMPÉRATIVE d'obtenir de l'information d'un tableau
+    // let nombreArticles =0 ;
+    // for(let i=0; i<articlesTab.length; i++) {
+    //     nombreArticles += articlesTab[i].qte;
+    // }
+    
+    //Façon DÉCLARATIVE d'obtenir de l'information d'un tableau
+    // const nombreArticles2 = articlesTab.reduce((acc, article) => acc + article.qte, 0);
+    // ou
+    // const nombreArticles2 = articlesTab.reduce(function (acc, article) { retunr : acc + article.qte } , 0);
+    
     return (
         <header className="Entete">
             <div className="logo">Logo</div>
@@ -13,7 +31,7 @@ function Entete(props) {
             </ul>
             <ul className="navUtilisateur">
                 <li>
-                    <Badge badgeContent={15} color="secondary">
+                    <Badge badgeContent={articlesTab.reduce((acc, article) => acc + article.qte, 0)} color="secondary">
                         <ShoppingCartIcon/>
                     </Badge>
                     </li>
