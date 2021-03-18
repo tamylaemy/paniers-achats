1fd01ebdb11cf6ad48094193aec0248267598da9
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-03-17 22:36:43 -0400

    Sauvegarde du panier (pas complété)

--- src/ConteneurGlobal.jsx
+++ src/ConteneurGlobal.jsx
@@ -9,8 +9,19 @@ import Contact from './Contact';
 import { Switch, Route } from 'react-router-dom';
 
 export default function ConteneurGlobal() {
+   // Vérifier s'il y a qqchose dans localStorage avec la clé 'panier-4pa'. 
+   let panier = {};
+   if(window.localStorage.getItem('panier-4pa')) {
+     panier = JSON.parse(window.localStorage.getItem('panier-4pa'));
+   }
+
   //Utiliser la gestion de l'état avec "useState"
   const etatPanier = useState({});
+  console.log("Le panier ressemble à ça :", etatPanier[0]);
+  console.log("Ce que retourne useState", etatPanier);
+
+  // Sauvegarder le panier dans localStorage à chq fois que l'état du panier est muté
+  window.localStorage.setItem('panier-4pa', JSON.stringify(etatPanier[0]));
 
   return (
     <div className="ConteneurGlobal">
